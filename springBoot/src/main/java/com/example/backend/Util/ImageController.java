fdee95442d355ff99c9943c506e25b47ad331144
NutThai <88415649+NutThai@users.noreply.github.com>
2023-10-05 23:56:30 +0700

    uploadImage

--- springBoot/src/main/java/com/example/backend/Util/ImageController.java
+++ springBoot/src/main/java/com/example/backend/Util/ImageController.java
@@ -1,7 +1,7 @@
 package com.example.backend.Util;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,23 +9,21 @@ import java.nio.file.Paths;
 
 @RestController
 public class ImageController {
-
-    @PostMapping(value = "/api/images/upload")
+    @PostMapping(value = "/images/upload")
     public String uploadImage(@RequestParam("image") MultipartFile image) {
-        return "Hello World";
-//        if (image.isEmpty()) {
-//            return "Please select a file to upload.";
-//        }
-//
-//        try {
-//            byte[] bytes = image.getBytes();
-//            String uploadPath = "../../resources/static"; // Specify your project folder path
-//            Path path = Paths.get(uploadPath + File.separator + image.getOriginalFilename());
-//            Files.write(path, bytes);
-//            return "Image uploaded successfully!";
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return "Image upload failed.";
-//        }
+        if (image.isEmpty()) {
+            return "Please select a file to upload.";
+        }
+        try {
+            byte[] bytes = image.getBytes();
+            String uploadPath = System.getProperty("user.dir")+"/src/main/resources/image/";
+//            System.out.println(uploadPath);
+            Path path = Paths.get(uploadPath +  image.getOriginalFilename());
+            Files.write(path, bytes);
+            return "Image uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Image upload failed.";
+        }
     }
 }
