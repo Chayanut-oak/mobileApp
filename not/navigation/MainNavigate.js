283e99623b762c6f8ef12f293cc90c993dd2379f
NutThai <88415649+NutThai@users.noreply.github.com>
2023-10-11 00:21:37 +0700

    Update MainNavigate.js

--- not/navigation/MainNavigate.js
+++ not/navigation/MainNavigate.js
@@ -23,58 +23,62 @@ const MainNavigate = () => {
     const auth = getAuth();
     const Mainnavigate = createNativeStackNavigator();
     const [user, setUserToken] = useState(null)
-
+    const storeUser = useSelector((state) => state.user)
+    const storeMeal = useSelector((state) => state.meal)
+    const storeIngredient = useSelector((state) => state.ingredient)
+    const [meals, setMeals] = useState(null)
+    const [users, setUsers] = useState(null)
+    const [ingredients, setIngredients] = useState(null)
     useEffect(() => {
         onAuthStateChanged(FIREBASE_AUTH, (user) => {
             try {
-                setUserToken(user ? user.stsTokenManager : null)
+                setUserToken(user ? user.stsTokenManager : null);
 
-                const allMealSnapshot = onSnapshot(collection(FIRE_STORE, "meals"), (collect) => {
-                    const allMeals = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-                    dispatch(saveMealData(allMeals))
+                const allMealSnapshot = onSnapshot(collection(FIRE_STORE, 'meals'), (collect) => {
+                    const allMeals = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+                    // Link ingredients to meals
+                    const linkedMeals = allMeals.map((meal) => {
+                        const linkedIngredients = meal.tags.map((ingredientId) => {
+                            return ingredients.find((ingredient) => ingredient.id === ingredientId);
+                        });
+                        return { ...meal, ingredients: linkedIngredients };
+                    });
+
+                    // Link createdBy to user
+                    const linkedMealsWithUsers = linkedMeals.map((meal) => {
+                        const createdByUser = users.find((user) => user.id === meal.createdBy);
+                        return { ...meal, createdBy: createdByUser };
+                    });
+
+                    setMeals(linkedMealsWithUsers);
                 }, (error) => {
-                    console.log(error)
+                    console.log(error);
                 });
 
-                const allIngredientSnapshot = onSnapshot(collection(FIRE_STORE, "ingredients"), (collect) => {
-                    const allIngredients = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-                    dispatch(saveIngredientData(allIngredients))
+                const allIngredientSnapshot = onSnapshot(collection(FIRE_STORE, 'ingredients'), (collect) => {
+                    const allIngredients = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+                    setIngredients(allIngredients);
                 }, (error) => {
-                    console.log(error)
-                });
-                const allUserSnapshot = onSnapshot(collection(FIRE_STORE, "users"), (collect) => {
-                    const allUsers = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-                    dispatch(saveUserData(allUsers))
-                }, (error) => {
-                    console.log(error)
+                    console.log(error);
                 });
 
-                // const mealDoc = doc(FIRE_STORE, "meals", mealId)
-
-
-                // const unsub0 = onSnapshot(mealDoc, (doc) => {
-                //     console.log(doc.data())
-                //     setMeal(doc.data())
-                // }, (error) => {
-                //     console.log(error)
-                // });
-
-                // const unsub1 = onSnapshot(doc(FIRE_STORE, "users", meal?.createdBy), (doc) => {
-                //   setMeal({ ...meal, createdBy: doc.data() })
-                // }, (error) =>{
-                //   console.log(error)
-                // });
-                // const createdByQuery = query()
-
-            }
-            catch (e) {
-                console.log(e)
+                const allUserSnapshot = onSnapshot(collection(FIRE_STORE, 'users'), (collect) => {
+                    const allUsers = collect.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+                    setUsers(allUsers);
+                }, (error) => {
+                    console.log(error);
+                });
+            } catch (e) {
+                console.log(e);
             }
             //query database 
 
         })
     }, [displayname])
-
+    console.log(meals)
+    console.log(users)
+    console.log(ingredients)
     return (
 
         <NavigationContainer>
