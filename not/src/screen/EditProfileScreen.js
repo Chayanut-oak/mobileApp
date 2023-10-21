fca356ee02fe2ccfa79b8931ae3864c829a109ec
NutThai <88415649+NutThai@users.noreply.github.com>
2023-10-21 13:40:43 +0700

    chage image profile

--- not/src/screen/EditProfileScreen.js
+++ not/src/screen/EditProfileScreen.js
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { updateProfile } from 'firebase/auth';
-import { FIREBASE_AUTH, FIRE_STORE } from '../../Firebaseconfig';
+import { FIREBASE_AUTH, FIRE_STORE, FIRE_STORAGE } from '../../Firebaseconfig';
+import { getDownloadURL, ref, uploadBytes, deleteObject } from 'firebase/storage';
 import { doc, updateDoc } from 'firebase/firestore';
-
+import { useSelector } from 'react-redux';
+import * as ImagePicker from 'expo-image-picker';
 // ตัวแปรที่เก็บอ้างอิงไปยังเอกสารของผู้ใช้ใน Firestore
 
 const auth = FIREBASE_AUTH;
 
 const EditProfileScreen = ({ navigation }) => {
+  const userStore = useSelector(state => state.user)
+  const [selectedImage, setSelectedImage] = useState(userStore.userImage.imagePath)
   const [user, setUser] = useState({
-    displayName:'',
+    displayName: '',
     firstName: '',
     lastName: '',
     email: '',
-    profileImage: '',
+    userImage: '',
   });
 
   const fetchUserData = async () => {
     const currentUser = auth.currentUser;
     if (currentUser) {
-      const { displayName, email, photoURL } = currentUser;
-      const [firstName, lastName] = displayName.split(' ');
+      const { displayName, email, userImage, firstName, lastName } = userStore;
+      const [] = displayName.split(' ');
       setUser({
         displayName,
         firstName,
         lastName,
         email,
-        profileImage: photoURL,
+        userImage: userImage,
       });
     }
   };
@@ -35,52 +39,102 @@ const EditProfileScreen = ({ navigation }) => {
 
   const handleUpdateProfile = async () => {
     try {
-      const newDisplayName = `${user.displayName}`;
-      const newFirstName = `${user.firstNameName}`;
-      const newLastName = `${user.lastNameName}`;
-      await updateProfile(auth.currentUser, { displayName: newDisplayName });
-      await updateProfile(auth.currentUser, { firstName: newFirstName });
-      await updateProfile(auth.currentUser, { lastNameName: newLastName });
-      
+      if (userStore.userImage.imagePath != selectedImage) {
+        if (userStore.userImage.imageName != null) {
+          const reference = ref(FIRE_STORAGE, '/userImages/' + userStore.userImage.imageName);
+          await deleteObject(reference);
+        }
+        uploadImage()
+      }
+      await updateProfile(auth.currentUser, { displayName: user.displayName, firstName: user.firstName, lastNameName: user.lastName });
       // อัปเดตข้อมูลใน Firestore ด้วยชื่อเอกสารของผู้ใช้
       await updateDoc(userDocRef, {
-        displayName: newDisplayName,
+        displayName: user.displayName,
         firstName: user.firstName,
-        lastName: user.lastName
+        lastName: user.lastName,
+        userImage: user.userImage
       });
-  
+      navigation.pop()
+      alert("แก้ไขโปรไฟล์สำเร็จ")
       // ส่งข้อมูลไปบันทึกใน Firebase Realtime Database หรือ Cloud Firestore
       // ตามที่คุณต้องการ
     } catch (error) {
       console.error('เกิดข้อผิดพลาดในการอัปเดตโปรไฟล์: ', error);
     }
   };
-
+  const uploadImage = async () => {
+    if (selectedImage != null) {
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          resolve(xhr.response);
+        };
+        xhr.onerror = function () {
+          reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', selectedImage, true);
+        xhr.send(null);
+      });
+      const filename = selectedImage.substring(selectedImage.lastIndexOf('/') + 1);
+      const imageStorageRef = ref(FIRE_STORAGE, 'userImages/' + filename);
+      await uploadBytes(imageStorageRef, blob, {
+        contentType: 'image/jpeg',
+      });
+      fetchImage(filename)
+    }
+  };
+  const fetchImage = async (filename) => {
+    try {
+      const reference = ref(FIRE_STORAGE, `/userImages/${filename}`);
+      const imageUrl = await getDownloadURL(reference);
+      setUser({ ...user, userImage: { imageName: filename, imagePath: imageUrl } })
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
+  };
   useEffect(() => {
     fetchUserData();
   }, []);
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={pickImage}>
+        <Image style={styles.image} source={selectedImage ? { uri: selectedImage } : require("../../picture/image.png")} />
+      </TouchableOpacity>
       <TextInput
+
         style={styles.textbox}
-        placeholder="Display name"
+        placeholder="Displayname"
         value={user.displayName}
         onChangeText={(text) => setUser({ ...user, displayName: text })}
       />
       <TextInput
         style={styles.textbox}
-        placeholder="First Name"
+        placeholder="ชื่อจริง"
         value={user.firstName}
         onChangeText={(text) => setUser({ ...user, firstName: text })}
       />
       <TextInput
         style={styles.textbox}
-        placeholder="Last Name"
+        placeholder="นามสกุล"
         value={user.lastName}
         onChangeText={(text) => setUser({ ...user, lastName: text })}
       />
       <Button
+        color="#DD2572"
         title="บันทึกข้อมูล"
         onPress={handleUpdateProfile}
       />
@@ -105,6 +159,13 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: 'white',
   },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 100
+  },
+
 });
 
 export default EditProfileScreen;
